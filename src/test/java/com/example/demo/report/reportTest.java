1eb0771db237f48dd1159e1cbec9aad6b59d2bc4
QIAN-GAO-AA <138557049+QIAN-GAO-AA@users.noreply.github.com>
2024-03-23 23:47:08 +0800

    feat(基础)://基础搭建

---
+++ src/test/java/com/example/demo/report/reportTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.report;
+
+
+import org.junit.jupiter.api.Test;
+
+public class reportTest {
+
+    @Test
+    public void dateTest(){
+        System.out.println("++++");
+    }
+}
